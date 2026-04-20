fd69695b3b83bd855ba5d27711b1e85a19a16fbb
Smart-coder872 <mmiller4328@gmail.com>
2026-04-19 21:05:45 -0400

    Notebook

--- Assignments/day4.py
+++ Assignments/day4.py
@@ -1,6 +1,5 @@
 from numpy import array, var, mean, sum, cov, random, sqrt
 
-class Day4:
 ## Exercise 1
 # A 3 variable matrix would probably be a 3x3 like this:
 # [Cov(X, X) Cov(X, Y) Cov(X, Z)
@@ -8,30 +7,45 @@ class Day4:
 #  Cov(Z, X) Cov(Z, Y) Cov(Z, Z)]
 
 ## Exercise 2
-    x = array([[0, 2], [1, 1], [2, 0]])
-    def x_setup(input):
-        n = len(input)
-        x_values = [xi[0] for xi in input]
-        y_values = [xi[1] for xi in input]
+x = array([[0, 2], [1, 1], [2, 0]])
+
+def covariance(X):
+    n =len(X)
+    for i in X:
+        x_all = X[i][0]
+        y_all = X[i][1]
+
+    print(x_all)
+covariance(x)
+
+
+def x_setup(input):
+    n = len(input)
+    x_values = [xi for xi in input[0:n-1][0]]
+    y_values = [input[xi][1] for xi in input]
         
-        for xi in input:
-            x_part = xi[0] - mean(x_values)
-            y_part = xi[1] - mean(y_values)
+    x_all = input[0:n-1][0]
+    y_all = input[0:n-1][1]
+        
+    x_part = x_values - mean(x_all)
+    y_part = y_values - mean(y_all)
             
-            variance = sum((x_part)*(y_part))/(n - 1)
-            print(variance)
+    variance = sum((x_part)*(y_part))/(n - 1)
+    
+    print(x_all)
 
 
-    #print(x_setup(x))
-    #print(cov(x))
+print(x_setup(x))
+#print(cov(x))
+
 ##Exercise 3
 rng = random.default_rng(seed=42)
 xarr = rng.random((3, 3))
 yarr = rng.random((3, 3))
 
-def coeff_func(x, y):
-    for value in x, y:
-        coeffs = cov(x, y)/(sqrt(cov(x)*cov(y)))
-        print(coeffs)
-coeff_func(xarr, yarr)
+#def coeff_func(x, y):
+    #for value in x, y:
+        #coeffs = cov(x, y)/(sqrt(cov(x)*cov(y)))
+        #print(coeffs)
+#coeff_func(xarr, yarr)
 
